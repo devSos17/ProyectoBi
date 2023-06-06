3a79f05399ec9b27ead5294a979da0e251209f03
Santiago Orozco <devsos117@gmail.com>
2023-06-05 23:59:29 -0600

    Add panel inicial para modulo contabilidad

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,22 +1,18 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+import PanelInicial from '@/Components/PanelInicial.vue';
 </script>
 
 <template>
     <AppLayout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
+                Menu Pricipal
             </h2>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
-                </div>
-            </div>
+        <div class="py-12 px-12">
+            <PanelInicial />
         </div>
     </AppLayout>
 </template>
