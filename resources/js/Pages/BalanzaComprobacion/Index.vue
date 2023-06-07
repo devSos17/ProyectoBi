22b1973c356963a74dec7dc2db0184d34a83bc9b
Santiago Orozco <devsos117@gmail.com>
2023-06-06 22:05:57 -0600

    Add total a balanza de comprobacion

--- resources/js/Pages/BalanzaComprobacion/Index.vue
+++ resources/js/Pages/BalanzaComprobacion/Index.vue
@@ -6,15 +6,21 @@
             filterDisplay="row"
             :class="`p-datatable-sm`"
         >
-            <!-- Texto por defecto en caso de que no exista -->
-            <template #empty>Sin Cuentas</template>
-
             <!-- Definir las columnas -->
-            <Column
-                field="nombre"
-                header="Cuenta"
-                style="min-width: 12rem"
-            ></Column>
+            <ColumnGroup type="header">
+                <Row >
+                    <Column header="Cuenta" :rowspan="2" />
+                    <Column header="Balanza de Movimientos" :colspan="2" />
+                    <Column header="Balanza de Saldos" :colspan="2" />
+                </Row>
+                <Row>
+                    <Column header="Abonos" style="min-width: 12rem" />
+                    <Column header="Retiros" style="min-width: 12rem" />
+                    <Column header="Abonos" style="min-width: 12rem" />
+                    <Column header="retiros" style="min-width: 12rem" />
+                </Row>
+            </ColumnGroup>
+            <Column field="nombre" style="min-width: 12rem"></Column>
             <Column
                 field="abonos"
                 header="Abonos"
@@ -26,15 +32,29 @@
                 style="min-width: 12rem"
             ></Column>
             <Column field="abonos" header="Abonos" style="min-width: 12rem">
-            <template #body="{ data }">
-                <span v-if="data.dif > 0">{{ data.abonos }}</span>
+                <template #body="{ data }">
+                    <span v-if="data.dif > 0">{{ data.abonos }}</span>
                 </template>
             </Column>
             <Column field="retiros" header="retiros" style="min-width: 12rem">
-            <template #body="{ data }">
-                <span v-if="data.dif < 0">{{ data.retiros }}</span>
+                <template #body="{ data }">
+                    <span v-if="data.dif < 0">{{ data.retiros }}</span>
                 </template>
             </Column>
+
+            <!-- Texto por defecto en caso de que no exista -->
+            <template #empty>Sin Cuentas</template>
+
+            <!-- Totales -->
+            <ColumnGroup type="footer">
+                <Row>
+                    <Column footer="Totales:" footerStyle="text-align:right" />
+                    <Column :footer="totalAbonos" />
+                    <Column :footer="totalRetiros" />
+                    <Column :footer="totalSaldoAbonos" />
+                    <Column :footer="totalSaldoRetiros" />
+                </Row>
+            </ColumnGroup>
         </DataTable>
     </AppLayout>
 </template>
@@ -49,6 +69,8 @@ import AppLayout from "@/Layouts/AppLayout.vue";
 // Componentes de PrimeVue
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
+import ColumnGroup from "primevue/columngroup"; // optional
+import Row from "primevue/row";
 
 // API para filtrar
 import { FilterMatchMode } from "primevue/api";
@@ -67,6 +89,23 @@ const cuentasValores = computed(() =>
             cuenta.dif = cuenta.abonos - cuenta.retiros;
             return cuenta;
         })
+);
+
+const totalAbonos = computed(() =>
+    cuentasValores.value.reduce((acc, val) => acc + val.abonos, 0)
+);
+const totalRetiros = computed(() =>
+    cuentasValores.value.reduce((acc, val) => acc + val.retiros, 0)
+);
+const totalSaldoAbonos = computed(() =>
+    cuentasValores.value
+        .filter((val) => val.dif > 0)
+        .reduce((acc, val) => acc + val.dif, 0)
+);
+const totalSaldoRetiros = computed(() =>
+    cuentasValores.value
+        .filter((val) => val.dif < 0)
+        .reduce((acc, val) => acc + -val.dif, 0)
 );
 
 // Métodos (Funciones)
